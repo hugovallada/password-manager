bd496335e04b79af0fbf20ac20311fd49f05c32a
Hugo Vallada <valladahugo@gmail.com>
2020-12-13 00:23:47 -0300

    Start password manager project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace password_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Password Manager!!! Version: 1.0");
+        }
+    }
+}
